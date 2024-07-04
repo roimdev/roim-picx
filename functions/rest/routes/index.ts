c0b6abf04febdb1e20cfc898af2a345374c8ac12
Nooniebao <java.lang.management@gmail.com>
2024-07-04 16:29:16 +0800

    s

--- functions/rest/routes/index.ts
+++ functions/rest/routes/index.ts
@@ -219,6 +219,7 @@ router.post("/folder",  auth, async (req: Request, env: Env) => {
 
         return json(Ok("Success"))
     } catch (e) {
+        console.log(e)
         return json(Fail("Create folder fail"))
     }
 })
