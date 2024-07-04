2343ae788f4e6fb88eee6434e64ea4a60cba64fa
Nooniebao <java.lang.management@gmail.com>
2024-07-04 15:20:27 +0800

    2

--- src/views/ManageImages.vue
+++ src/views/ManageImages.vue
@@ -9,8 +9,8 @@
 					已上传 {{ uploadedImages.length }} 张图片，共 {{ formatBytes(imagesTotalSize) }}
 				</div>
 			</div>
-      <div class="flex items-center justify-start" v-if="0">
-        <font-awesome-icon :icon="faFolderPlus" class="text-xl cursor-pointer text-3xl text-amber-300 mr-2" @click="addFolder" />
+      <div class="flex items-center justify-start" >
+        <font-awesome-icon :icon="faFolderPlus" v-if="0" class="text-xl cursor-pointer text-3xl text-amber-300 mr-2" @click="addFolder" />
         <font-awesome-icon
             :icon="faRedoAlt"
             class="text-xl cursor-pointer text-indigo-400"
